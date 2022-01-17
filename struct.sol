2d1e4462fc1c8af4177dcb8de8b6adba7aa05376
tbw875 <charger875@gmail.com>
2022-01-17 13:23:18 -0800

    new branch with onlyOwner testing

--- struct.sol
+++ struct.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.8.7;
 //SPDX-License-Identifier: MIT
 
 contract myContract{
-
     uint256 public peopleCount;
-
     mapping(uint => Person) public people; //uint is key, Person is value
+
+    address owner;
 
     struct Person {
         uint _id;
@@ -13,7 +13,7 @@ contract myContract{
         string _lastname;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public{
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner{
         incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
     }
